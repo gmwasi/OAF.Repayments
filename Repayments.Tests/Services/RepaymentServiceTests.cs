adb316d3ef6b990d1293770d4372ba266b1e5f20
Brian Mwasi <>
2020-03-26 10:13:36 +0300

    final

---
+++ Repayments.Tests/Services/RepaymentServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Repayments.Core.Services;
+
+using NUnit.Framework;
+
+namespace Repayments.Tests.Services
+{
+    [TestFixture]
+    public class RepaymentServiceTests
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void should_return_amount_owed()
+        {
+            
+        }
+
+    }
+}
--- Repayments.Web/ClientApp/src/app/submit-repayment/submit-repayment.component.ts
+++ Repayments.Web/ClientApp/src/app/submit-repayment/submit-repayment.component.ts
@@ -1,39 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import {
+  HttpClient,
+  HttpRequest,
+  HttpEventType,
+  HttpResponse
+} from "@angular/common/http";
 
 @Component({
-  selector: 'app-submit-repayment',
-  templateUrl: './submit-repayment.component.html',
-  styleUrls: ['./submit-repayment.component.css']
+  selector: "app-submit-repayment",
+  templateUrl: "./submit-repayment.component.html",
+  styleUrls: ["./submit-repayment.component.css"]
 })
 export class SubmitRepaymentComponent implements OnInit {
-
   public progress: number;
   public message: string;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  selectedFile: File;
+
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsText(this.selectedFile, "UTF-8");
+    fileReader.onload = () => {
+      console.log(JSON.parse(this.arrayBufferToString(fileReader.result)));
+    };
+    fileReader.onerror = error => {
+      console.log(error);
+    };
+  }
+
+  arrayBufferToString(buffer) {
+    var arr = new Uint8Array(buffer);
+    var str = String.fromCharCode.apply(String, arr);
+    if (/[\u0080-\uffff]/.test(str)) {
+      throw new Error(
+        "this string seems to contain (still encoded) multibytes"
+      );
+    }
+    return str;
+  }
+
+  onUpload() {
+    // upload code goes here
+  }
 
   upload(files) {
-    if (files.length === 0)
-      return;
+    if (files.length === 0) return;
 
     const formData = new FormData();
 
-    for (let file of files)
-      formData.append(file.name, file);
+    for (let file of files) formData.append(file.name, file);
 
-    const uploadReq = new HttpRequest('POST', `api/upload`, formData, {
-      reportProgress: true,
+    const uploadReq = new HttpRequest("POST", `repay`, formData, {
+      reportProgress: true
     });
 
     this.http.request(uploadReq).subscribe(event => {
       if (event.type === HttpEventType.UploadProgress)
-        this.progress = Math.round(100 * event.loaded / event.total);
+        this.progress = Math.round((100 * event.loaded) / event.total);
       else if (event.type === HttpEventType.Response)
         this.message = event.body.toString();
     });
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
